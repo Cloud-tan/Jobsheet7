97a0fb73f54b50758c009b21c8ee9639804d3c08
Cloudtan <maranathajeremy@gmail.com>
2025-11-19 02:55:10 +0700

    good grade

--- SiakadWhile11.java
+++ SiakadWhile11.java
@@ -4,25 +4,26 @@ public class SiakadWhile11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int grade, total;
+        int grade, total = 0;
         int i = 0;
-
-        System.out.print("Enter The Number Of Students:   ");
-        total = sc.nextInt();
 
         while (i < total) {
             i++;
         }
+        System.out.print("Enter The Number Of Students:   ");
+        total = sc.nextInt();
+        
         while (i < total) {
-            System.out.println("Enter The Grade Of The " + (i + 1) + " Student:    ");
+            System.out.print("Enter The Grade Of The " + (i + 1) + " Student:    ");
             grade = sc.nextInt();
 
         if (grade < 0 || grade > 100) {
-            System.out.println("Invalid Grade. Re-enter a valid grade! ");
+            System.out.println("~~~~~~ Invalid Grade. Re-enter a valid grade! ~~~~~~");
             continue;
             
         } if (grade > 80 && grade <= 100) {
             System.out.println("The Grade of the " + (i + 1) + " Student Is A ");
+            System.out.println("~~~~~~~~~ Good, keep the grade! ~~~~~~~~~"); // Added motivational message  
         } else if (grade > 73 && grade <= 80) {
             System.out.println( "The Grade of the " + (i + 1) + " Student Is B+ " );
         } else if (grade > 65 && grade <= 73) {
@@ -37,7 +38,7 @@ public class SiakadWhile11 {
             System.out.println( "The Grade of the " + (i + 1) + " Student Is E " );
         } i++;
         } 
-
+    
         sc.close();
     }
 }
