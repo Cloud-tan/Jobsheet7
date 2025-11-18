a7d54bbca3300e8ad7eac4c3458938f191cb60b2
Cloudtan <maranathajeremy@gmail.com>
2025-11-18 13:25:39 +0700

    change1

---
+++ KafeDoWhile11.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class KafeDoWhile11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int coffe, tea, bread, totalPrice;
+        String customerName;
+        int coffeePrice = 12000;
+        int teaPrice = 7000;
+        int breadPrice = 20000;
+        
+
+        do {
+            System.out.print("Enter Customer Name:  ");
+            customerName = sc.nextLine();
+            if (customerName.equalsIgnoreCase("cancel")) {
+                System.out.println("Transaction Cancelled.");
+                break;
+            }
+
+            System.out.print("Enter The Number Of Coffees:   ");
+            coffe = sc.nextInt();
+            System.out.print("Enter The Number Of Teas:  ");
+            tea = sc.nextInt();
+            System.out.print("Enter The Number Of Breads:   ");
+            bread = sc.nextInt();
+            
+            totalPrice = (coffe * coffeePrice) + (tea * teaPrice) + (bread * breadPrice);
+            System.out.println("Total To Be Paid: Rp " + totalPrice);
+            sc.nextLine();
+        } while (false); 
+
+        System.out.println("All Transactions Completed.");
+        
+        sc.close(); 
+    }
+}
